ba07976d69139b60e42f08207942cc137011bebc
luidimso <luidi_mso@outlook.com>
2020-11-23 10:33:56 -0300

    Future builder

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,40 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.amber,
         centerTitle: true,
       ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch(snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: Text("Loading...",
+                  style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 25
+                  ),
+                  textAlign: TextAlign.center
+                ),
+              );
+            default:
+              if(snapshot.hasError) {
+                return Center(
+                  child: Text("Error :(",
+                      style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 25
+                      ),
+                      textAlign: TextAlign.center
+                  ),
+                );
+              } else {
+                return Container(
+                  color: Colors.green,
+                );
+              }
+          }
+        },
+      ),
     );
   }
 }
