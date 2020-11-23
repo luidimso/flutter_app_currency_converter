c3f356faddb5ffe592f80b2ac9e5f090b3d8ea2a
luidimso <luidi_mso@outlook.com>
2020-11-23 10:02:49 -0300

    Future data

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const api_url = "https://api.hgbrasil.com/finance?key=cb44cfd2";
 
 void main() async {
-  http.Response response = await http.get(api_url);
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
+  print(await getData());
 
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
   ));
 }
+
+Future<Map> getData() async {
+  http.Response response = await http.get(api_url);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Converter \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+    );
+  }
+}
+
