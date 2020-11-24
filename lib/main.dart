3510fa10248f1d116bb336006a1b5bfe6292e997
luidimso <luidi_mso@outlook.com>
2020-11-24 09:33:26 -0300

    Building interface

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,19 @@ import 'dart:convert';
 const api_url = "https://api.hgbrasil.com/finance?key=cb44cfd2";
 
 void main() async {
-  print(await getData());
-
   runApp(MaterialApp(
     home: Home(),
+    theme: ThemeData(
+      hintColor: Colors.amber,
+      primaryColor: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+        enabledBorder:
+        OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        focusedBorder:
+        OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+        hintStyle: TextStyle(color: Colors.amber),
+      )
+    ),
   ));
 }
 
@@ -24,6 +33,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double dollar;
+  double bitcoin;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,8 +72,64 @@ class _HomeState extends State<Home> {
                   ),
                 );
               } else {
-                return Container(
-                  color: Colors.green,
+                dollar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                bitcoin = snapshot.data["results"]["currencies"]["BTC"]["buy"];
+
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(Icons.monetization_on,
+                        color: Colors.amber,
+                        size: 150,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: "Real",
+                          labelStyle: TextStyle(
+                            color: Colors.amber
+                          ),
+                          border: OutlineInputBorder(),
+                          prefixText: "R\$"
+                        ),
+                        style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 25
+                        ),
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: "Dollar",
+                            labelStyle: TextStyle(
+                                color: Colors.amber
+                            ),
+                            border: OutlineInputBorder(),
+                            prefixText: "US\$"
+                        ),
+                        style: TextStyle(
+                            color: Colors.amber,
+                            fontSize: 25
+                        ),
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                            labelText: "Bitcoin",
+                            labelStyle: TextStyle(
+                                color: Colors.amber
+                            ),
+                            border: OutlineInputBorder(),
+                            prefixText: "BTC"
+                        ),
+                        style: TextStyle(
+                            color: Colors.amber,
+                            fontSize: 25
+                        ),
+                      )
+                    ],
+                  ),
                 );
               }
           }
